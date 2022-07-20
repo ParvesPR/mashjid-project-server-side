5733d1a4d8173c0e1629cbe3f7123ed3f9525e0a
ParvesPR <parves2585@gmail.com>
2022-07-20 23:38:33 +0800

    created api for find admin user only

--- index.js
+++ index.js
@@ -93,6 +93,14 @@ async function run() {
             res.send(result);
         });
 
+        // FIND ADMIN USER
+        app.get('/admin/:email', verifyJwt, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
+        });
+
         // CREATE A NEW POST
         app.post('/blogs', async (req, res) => {
             const newBlog = req.body;
